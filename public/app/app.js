4a92e00476f4b5b132c01dfc5277920cebc85e0e
rafamagalhas <magalhas.rafa@gmail.com>
2023-04-01 14:32:22 -0300

    feat: add handle status function

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,9 @@
+import { handleStatus } from "./utils/promise-helpers.js";
+
 document
 .querySelector('#myButton')
 .onclick = () =>
-  fetch('http://localhost:3000/notasx')
-    .then(res => {
-      if (res.ok) return res.json;
-      return Promise.reject(res.statusText)
-    })
+  fetch('http://localhost:3000/notas')
+    .then(handleStatus)
     .then(notas => console.log(notas))
     .catch(console.log);
