f362efa79847c3ca441ee6d41f09d81ff1c94031
rafamagalhas <magalhas.rafa@gmail.com>
2023-04-21 20:31:21 -0300

    feat: add isolation method using event emitter

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,11 @@
 import { notasService as service } from "./nota/service.js";
 import { debounceTime, takeUntil } from "./utils/operators.js";
+import { EventEmitter } from "./utils/event-emitter.js";
 
 const action = debounceTime(
   500,
   takeUntil(3, () =>
-    service.sumItems("2143").then(console.log).catch(console.log)
+    service.sumItems("2143").then(total => EventEmitter.emit('itensTotalizados', total)).catch(console.log)
   )
 );
 
