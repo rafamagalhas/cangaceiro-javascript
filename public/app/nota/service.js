ed8e0461cedc88029fe25da9b663bb9b30869ac8
rafamagalhas <magalhas.rafa@gmail.com>
2023-04-22 15:53:49 -0300

    feat: add monad design pattern

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,20 +1,25 @@
+import { Maybe } from "../utils/maybe.js";
 import { partialize, compose } from "../utils/operators.js";
 import { handleStatus } from "../utils/promise-helpers.js";
 
 const API = "http://localhost:3000/notas";
 
-const getItemsFromNotas = (notas) => notas.flatMap((nota) => nota.itens);
+const getItemsFromNotas = (notasMonad) =>
+  notasMonad.map((notas) => notas.flatMap((nota) => nota.itens));
 
-const filterItemsByCode = (code, items) =>
-  items.filter((item) => item.codigo === code);
+const filterItemsByCode = (code, itemsMonad) =>
+  itemsMonad.map((items) => items.filter((item) => item.codigo === code));
 
-const sumItemsValue = (itens) =>
-  itens.reduce((total, item) => total + item.valor, 0);
+const sumItemsValue = (itemsMonad) =>
+  itemsMonad.map((items) =>
+    items.reduce((total, item) => total + item.valor, 0)
+  );
 
 export const notasService = {
   listAll() {
     return fetch(API)
       .then(handleStatus)
+      .then((notas) => Maybe.of(notas))
       .catch((err) => {
         console.log(err);
         return Promise.reject("Não foi possível obter as notas fiscais");
@@ -27,6 +32,8 @@ export const notasService = {
       getItemsFromNotas
     );
 
-    return this.listAll().then(sumItems);
+    return this.listAll()
+      .then(sumItems)
+      .then((result) => result.getOrElse(0));
   },
 };
